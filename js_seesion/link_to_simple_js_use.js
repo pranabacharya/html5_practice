311e57a306423ed7b9c2419e36335c31047ae626
pranabacharya <pacharya808@gmail.com>
2020-06-27 00:40:24 +0530

    created external js and few changes to my previous html and css

---
+++ js_seesion/link_to_simple_js_use.js
@@ -0,0 +1,67 @@
+//1. Function Practice
+// function add(a, b){
+//         c = a+b;
+//         return c;
+//     }
+
+//         c1 = add(4,5);
+//         c2 = add(6,5);
+//         document.write(c1);
+//         document.write(c2);
+
+
+//2. Function Practice
+//  function add(x ,y) {
+//      z = (x/y)*100 ;
+// return z;
+// }
+
+
+// z1 = add(55,5);
+// z2 = add(333,3);
+// z3 = add(555,5);
+// z4 = add(55555,55555);
+
+// console.log(z1,z2,z3,z4);
+// document.write(z1,z2,z3,z4);
+// alert(z4);
+
+//3. Function Practice----Loop
+// var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
+// var arr = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+// console.log (arr);
+// for(var i = 0; i<arr.length; i++){
+//     console.log(arr[i]);
+//     document.writeln(arr[i]);
+// console.log(arr.length);
+// }
+// console.log(arr);
+// document.writeln(arr.length);
+// for(var i = 0; i<arr.length; i++){
+//     console.log(arr[i]);
+//     document.writeln(arr[i]);
+// }
+
+//4.Function Practice---- ForEach Loop
+// var array = [1,2,3,4,5,6,7]
+// // array.forEach(function(value){
+// //     console.log(value);
+// //     document.writeln(value);
+// // })
+// console.log(array[5]);
+
+// var arr = ['L', 'U', 'C', 'K', 'Y'];
+
+// arr.forEach(function (data) {
+//     console.log(data);
+//     document.writeln(data);
+// })
+
+// String Methods
+
+let mystr = "Pranab is a good boy.";
+
+// MahString.replace("good","bad");
+d = mystr.replace ("Pranab","Lucky");
+
+console.log(d, mystr);
